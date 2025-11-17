57fcdc9ab2f6f7c1d1840cafa3e077dcded0b1d0
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-11-17 15:42:27 -0700

    added some validators to the form

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,15 @@
 const showPassword = document.querySelector("#show-password");
 
-showPassword.addEventListener("click", function() {
-  const passwordInput = document.getElementById("account_password");
-  const type = passwordInput.getAttribute("type");
-  if (type == "password") {
-    passwordInput.setAttribute("type", "text");
-    showPassword.InnerHTML = "Hide Password";
-  } else {
-    passwordInput.setAttribute("type", "password");
-    showPassword.innerHTML = "Show Password";
-  }
-});
+if (showPassword) {
+    showPassword.addEventListener("click", function() {
+    const passwordInput = document.getElementById("account_password");
+    const type = passwordInput.getAttribute("type");
+    if (type == "password") {
+      passwordInput.setAttribute("type", "text");
+      showPassword.InnerHTML = "Hide Password";
+    } else {
+      passwordInput.setAttribute("type", "password");
+      showPassword.innerHTML = "Show Password";
+    }
+  })
+};
